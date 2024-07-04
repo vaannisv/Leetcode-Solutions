8e9581add087b8cf34314372c2fbf7215527ac84
Vanni <vaanichaudhary666@gmail.com>
2024-07-04 22:24:57 +0530

    Day7
    Did 2 easy questions

---
+++ 1-Easy/35-Search_insert_position.cpp
@@ -0,0 +1,60 @@
+/*Question no.35
+Search Insert Position
+
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104*/
+
+//Solution:
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n= nums.size();
+        int s= 0;
+        int e= n-1;
+        int mid= s+(e-s)/2;
+
+        while(s<=e)
+        {
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            if(target > nums[mid])
+            {
+                s= mid+1;
+            }
+            else{
+                e= mid-1;
+            }
+            mid= s+(e-s)/2;
+        }
+        return s;
+    }
+};
